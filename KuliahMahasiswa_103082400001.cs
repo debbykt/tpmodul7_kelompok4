e13435cf132136f8f465e5ca3d56d58e3c6ce53f
Miranisa3113 <miratunn91@gmail.com>
2026-04-21 19:53:40 +0700

    menambahkan class KuliahMahasiswa_103082400001

---
+++ KuliahMahasiswa_103082400001.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+
+public class KuliahMahasiswa103082400001
+{
+    public class MataKuliah
+    {
+        public string kode_matakuliah { get; set; }
+        public string nama_matakuliah { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        string jsonString = File.ReadAllText("tp7_2_103082400001.json");
+
+        List<MataKuliah> data =
+            JsonSerializer.Deserialize<List<MataKuliah>>(jsonString);
+
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+
+        int i = 1;
+        foreach (var mk in data)
+        {
+            Console.WriteLine($"MK {i} {mk.kode_matakuliah} - {mk.nama_matakuliah}");
+            i++;
+        }
+    }
+}
