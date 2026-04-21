35b6d45f94acb326409aecac1a71a811a40cd825
Rouzok <98026745+Rouzok@users.noreply.github.com>
2026-04-21 20:03:20 +0700

    menambahkan class DataMahasiswa

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine();
+﻿using tpmodul7_kelompok_4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa_103082400009 dataMahasiswa = new DataMahasiswa_103082400009();
+        dataMahasiswa.ReadJSON();
+    }
+}
