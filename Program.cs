d7d517fa8056ade287a46b2a303ce83f8de2ca59
Rouzok <98026745+Rouzok@users.noreply.github.com>
2026-04-21 20:40:03 +0700

    perbaikan program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿using tpmodul7_kelompok_4;
+using static tpmodul7_kelompok_4.KuliahMahasiswa103082400009;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         DataMahasiswa_103082400009 dataMahasiswa = new DataMahasiswa_103082400009();
-        KuliahMahasiswa103082400009 kuliahMahasiswa = new KuliahMahasiswa103082400009();
+        KuliahMahasiswa_103082400009 kuliahMahasiswa = new KuliahMahasiswa_103082400009();
 
+        Console.WriteLine("===DataMahasiswa===");
         dataMahasiswa.ReadJSON();
+        Console.WriteLine();
+        Console.WriteLine("===KuliahMahasiswa===");
         kuliahMahasiswa.ReadJSON();
     }
 }
